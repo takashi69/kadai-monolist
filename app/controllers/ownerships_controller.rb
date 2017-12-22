e101543bd3eecd3d523558815addbc7f6033555e
Takashi Matsumoto <taka6276matu@gmail.com>
2017-12-22 05:26:02 +0000

    first commit

---
+++ app/controllers/ownerships_controller.rb
@@ -0,0 +1,44 @@
+class OwnershipsController < ApplicationController
+  def create
+    @item = Item.find_or_initialize_by(code: params[:item_code])
+    
+    unless  @item.persisted?
+      results = RakutenWebService::Ichiba::Item.search(itemCode: @item.code)
+      
+      @item = Item.new(read(results.first))
+      @item.save
+    end
+    
+    if params[:type] == 'Want'
+      current_user.want(@item)
+      flash[:success] = '商品をWantしました'
+    end
+    
+    # haveのをここで作成。
+    if params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品をHaveしました'
+    end
+    
+    redirect_back(fallback_location: root_path)
+    
+  end
+
+  def destroy
+    @item = Item.find(params[:item_id])
+    
+    if params[:type] == 'Want'
+      current_user.unwant(@item)
+      flash[:success] = '商品をWantを解除しました'
+    end
+    
+    # haveのdestroyを設定
+    
+    if params[:type] == 'Have'
+      current_user.unhave(@item)
+      flash[:success] = '商品をHave解除しました'
+    end
+    
+    redirect_back(fallback_location: root_path)
+  end
+end
